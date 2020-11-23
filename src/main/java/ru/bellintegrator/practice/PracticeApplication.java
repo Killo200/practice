b008521103582fa8741ddd275ec668d936f39b03
Killo200 <gauss-nsk@yandex.ru>
2020-11-23 20:31:43 +0300

    Добавлена зависимость Swagger

--- src/main/java/ru/bellintegrator/practice/PracticeApplication.java
+++ src/main/java/ru/bellintegrator/practice/PracticeApplication.java
@@ -2,7 +2,22 @@ package ru.bellintegrator.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Locale;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
 @SpringBootApplication
 public class PracticeApplication {
 
@@ -10,4 +25,29 @@ public class PracticeApplication {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("ru.bellintegrator.practice"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Spring REST Sample with Swagger")
+				.description("Spring REST Sample with Swagger")
+				.contact("https://github.com/Killo200/practice")
+				.version("1.0")
+				.build();
+	}
 }
--- src/main/java/ru/bellintegrator/practice/controller/organization/OrganizationController.java
+++ src/main/java/ru/bellintegrator/practice/controller/organization/OrganizationController.java
@@ -1,5 +1,7 @@
 package ru.bellintegrator.practice.controller.organization;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Api(value = "OrganizationController", description = "Управление информацией об организациях")
 @RestController
 @RequestMapping(value = "/api/organization", produces = APPLICATION_JSON_VALUE)
 public class OrganizationController {
@@ -22,6 +25,7 @@ public class OrganizationController {
         this.organizationService = organizationService;
     }
 
+    @ApiOperation(value = "Получить список всех организаций", httpMethod = "GET")
     @GetMapping("/organizations")
     public List<Organization> all() {
         return organizationService.all();
